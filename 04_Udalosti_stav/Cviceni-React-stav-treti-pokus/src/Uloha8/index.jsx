b81f9453d5cebd1a510d3f85387d5d7954c61c9d
Ivuska <fischerova.ivka@gmail.com>
2023-03-06 14:24:39 +0100

    Reseni Ulohy 8.

--- 04_Udalosti_stav/Cviceni-React-stav-treti-pokus/src/Uloha8/index.jsx
+++ 04_Udalosti_stav/Cviceni-React-stav-treti-pokus/src/Uloha8/index.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Zadání 1: Vytvoř stavovou proměnnou s výchozí hodnotou `'možná'`.
 // Zadání 2: Proměnnou vypiš místo `ano/ne/možná`.
 // Zadání 3: Po kliknutí na tlačítko změň `možná` na `ano`, `ano` na `ne`, případně `ne` na `možná`.
 
 export const Uloha8 = () => {
+	const [destVBrne, setDestVBrne] = useState('možná');
+
+	const zmen = () => {
+		if (destVBrne === 'možná') {
+			setDestVBrne('ano');
+		} else if (destVBrne ==='ano') {
+			setDestVBrne('ne');
+		} else {
+			setDestVBrne('možná');
+		}
+	}
+
 	return (
 		<>
-			<h3>Prší v Brně: ano/ne/možná</h3>
-			<button>změnit</button>
+			<h3>Prší v Brně: {destVBrne}</h3>
+			<button onClick={(zmen)}>změnit</button>
 		</>
 	)
 }
