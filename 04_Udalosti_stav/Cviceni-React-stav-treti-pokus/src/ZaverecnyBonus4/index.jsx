9afa8ea7c4302909955f3997ce0af19227992de4
Ivuska <fischerova.ivka@gmail.com>
2023-03-06 14:54:32 +0100

    reseni Zaverecneho bonusu c.4.

--- 04_Udalosti_stav/Cviceni-React-stav-treti-pokus/src/ZaverecnyBonus4/index.jsx
+++ 04_Udalosti_stav/Cviceni-React-stav-treti-pokus/src/ZaverecnyBonus4/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Zadání: Pomocí dvou stavových proměnných s výchozí hodnotou 0, které se budou měnit podle vstupních políček, vypiš do `<output></output>` jejich součet.
 
 export const ZaverecnyBonus4 = () => {
+	const [numberOne, setNumberOne] = useState(0);
+	const [numberTwo, setNumberTwo] = useState(0);
+
+	//const soucet = numberOne + numberTwo
+
 	return (
 		<>
-			<input type="number" defaultValue="0" /> +{' '}
-			<input type="number" defaultValue="0" /> = <output>0</output>
+			<input type="number" defaultValue="0" onChange={() => setNumberOne(Number(event.target.value))}/> +{' '}
+			<input type="number" defaultValue="0" onChange={() => setNumberTwo(Number(event.target.value))}/> = <output> {numberOne + numberTwo} </output>
 		</>
 	)
 }
