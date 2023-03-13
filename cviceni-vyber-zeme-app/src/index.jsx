e56f142f77939f7e7f29e17798f28c850cf33330
Ivuska <fischerova.ivka@gmail.com>
2023-03-13 10:06:00 +0100

    Reseni cviceni Formularove prvky - zasilani newsletteru (checkbox a hodnota ve stavu).

--- cviceni-vyber-zeme-app/src/index.jsx
+++ cviceni-vyber-zeme-app/src/index.jsx
@@ -9,9 +9,11 @@ const App = () => {
 
   const [country, setCountry] = useState('Česká republika');
 
+  const [newsletterAccepted, setNewsletterAccepted] = useState(false)
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Uzivatel ${userName} ze zeme ${country}`);
+    alert(`Uzivatel ${userName} ze zeme ${country} a ${newsletterAccepted ? "chce dostavat pravidelne novinky" : "o novinky nema zajem"}`);
   };
 
   return (
@@ -30,15 +32,24 @@ const App = () => {
       </label>
       <label>
     Země původu:
-    <select 
-      value = {country}
-      onChange={(e) => setCountry(e.target.value)}>
-      <option value="Chorvatsko">Chorvatsko</option>
-      <option value="Česká republika">Česká republika</option>
-      <option value="Polsko">Polsko</option>
-      <option value="Slovenská republika">Slovenská republika</option>
-    </select>
-  </label>
+        <select 
+          value = {country}
+          onChange={(e) => setCountry(e.target.value)}>
+          <option value="Chorvatsko">Chorvatsko</option>
+          <option value="Česká republika">Česká republika</option>
+          <option value="Polsko">Polsko</option>
+          <option value="Slovenská republika">Slovenská republika</option>
+        </select>
+      </label>
+      <label>
+        <input 
+          type="checkbox" 
+          checked={newsletterAccepted}
+          // u checkboxu neni value, ale checked, proto jej musim nastavit i v onChange()
+          onChange={(e) => setNewsletterAccepted(e.target.checked)}
+          />
+          Chci pravidelně dostávat novinky do mé e-mailové schránky.
+      </label>
       <button type="submit" disabled={userName === '' ? true : false}>Registrovat</button>
     </form>
   );
