287c45341f7bbc64a36a6bb690851a8b52f42b72
Ivuska <fischerova.ivka@gmail.com>
2023-03-16 20:15:14 +0100

    Reseni ukolu z lekce komunikace.

---
+++ 06_Komunikace_dite_rodic/volby/src/Candidate/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+const Candidate = ({ name, avatar, onVote }) => {
+
+  const handleClick = () => {
+    onVote(name, avatar)
+  }
+
+  return (
+    <div className="candidate">
+      <h3 className="candidate__name">{name}</h3>
+      <img className="candidate__avatar" src={avatar} />
+      <button className="btn-vote" onClick={handleClick}>Vybrat</button>
+    </div>
+  )
+  };
+
+export default Candidate;
