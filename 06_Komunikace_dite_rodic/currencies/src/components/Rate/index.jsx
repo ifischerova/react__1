9bc3e0a0d5f1c8bc4710d9d594fb0b35a2e48c81
Ivuska <fischerova.ivka@gmail.com>
2023-03-16 20:14:31 +0100

    Reseni prvnich dvou cviceni ze 'Cviceni: Komunikace'.

---
+++ 06_Komunikace_dite_rodic/currencies/src/components/Rate/index.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+//const currencies = {
+  //USD: { CZK: 23.823 },
+  //EUR: { CZK: 24.74 },
+  //GBP: { CZK: 29.067 }
+//};
+
+const Rate = ({from}) => {
+  const [ toCurrency, setToCurrency ] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+      .then((response) => {
+          return response.json();
+      })
+      .then((data) => {
+          setToCurrency(data.rates.CZK)
+      })
+  }, [from])
+
+  return (
+    <div className="rate">
+      <div className="rate__currency">1 {from}</div>
+      <div>=</div>
+      <div className="rate__value" >{toCurrency}CZK</div>
+    </div>
+  );
+};
+
+export default Rate;
